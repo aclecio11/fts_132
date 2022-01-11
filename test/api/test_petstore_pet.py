f1b91c5759e24abfcd346c961516a09b4c4b153c
ACLECIO PEREIRA DOS SANTOS <aclecio11@gmail.com>
2022-01-11 10:17:15 -0300

    Consulta(GET) do teste logar usuario e extração do token do ususario

---
+++ test/api/test_petstore_pet.py
@@ -0,0 +1,8 @@
+# Bibliotecas:
+
+import pytest  # Framework de Teste  unitario - Engine
+import requests  # Framework de Teste de API - Requests / Responses
+
+# Endereço da API:
+base_url = 'https://petstore.swagger.io/v2/pet'
+headers = {'Content-Type': 'application/json'}  # Nos .asmx seria 'text/xml'
--- test/api/test_petstore_user.py
+++ test/api/test_petstore_user.py
@@ -3,6 +3,8 @@
 import pytest  # Framework de Teste  unitario - Engine
 import requests  # Framework de Teste de API - Requests / Responses
 
+
+
 # Endereço da API:
 base_url = 'https://petstore.swagger.io/v2/user'
 headers = {'Content-Type': 'application/json'}  # Nos .asmx seria 'text/xml'
@@ -57,21 +59,27 @@ def testar_consultar_usuario():
     )
 
     # 3 Formatação:
-    corpo_da_resposta = resposta.json()
-    print(resposta)  # Resposta bruta
-    print(resposta.status_code)  # Status code
-    print(corpo_da_resposta)  # Resposta formatada
+    match resposta.status_code:
+        case 200:
+            corpo_da_resposta = resposta.json()
+            print(resposta)  # Resposta bruta
+            print(resposta.status_code)  # Status code
+            print(corpo_da_resposta)  # Resposta formatada
 
-    # 4 Valida:
-    assert resposta.status_code == status_code
-    assert corpo_da_resposta['id'] == id
-    assert corpo_da_resposta['username'] == username
-    assert corpo_da_resposta['firstName'] == firstName
-    assert corpo_da_resposta['lastName'] == lastName
-    assert corpo_da_resposta['email'] == email
-    assert corpo_da_resposta['password'] == password
-    assert corpo_da_resposta['phone'] == phone
-    assert corpo_da_resposta['userStatus'] == userStatus
+            # 4 Valida:
+            assert resposta.status_code == status_code
+            assert corpo_da_resposta['id'] == id
+            assert corpo_da_resposta['username'] == username
+            assert corpo_da_resposta['firstName'] == firstName
+            assert corpo_da_resposta['lastName'] == lastName
+            assert corpo_da_resposta['email'] == email
+            assert corpo_da_resposta['password'] == password
+            assert corpo_da_resposta['phone'] == phone
+            assert corpo_da_resposta['userStatus'] == userStatus
+        case 400:
+            print('username fornecido incorretamente')
+        case 404:
+            print('usuario não encontrado')
 
 
 ##################################################################################
@@ -119,15 +127,54 @@ def testar_excluir_usuario():
         headers=headers  # O header com o Content type
     )
     # Formatação:
+    match resposta.status_code: # nao usar
+        case 200:      # Apagar um usuario encontrado
+            corpo_da_resposta = resposta.json()
+            print(resposta)  # Resposta bruta
+            print(resposta.status_code)  # Status code
+            print(corpo_da_resposta)  # Resposta formatada
+
+            # Valida:
+            assert resposta.status_code == status_code_esperado
+
+            assert corpo_da_resposta['code'] == code_esperado
+            assert corpo_da_resposta['type'] == type_esperado
+            assert corpo_da_resposta['message'] == mensagem_esperada
+        case 400: # na pratica nao usar match e case:
+            print('username fornecido incorretamente')
+        case 404:
+            print('usuario não encontrado')
+##################################################################################
+
+def testar_login_do_usuario():
+    #1 Configura:
+    username = 'aclecio'
+    password = '123456'
+    status_code_esperado = 200
+    code_esperado = 200
+    type_esperado = 'unknown'
+    inicio_mensagem_esperada = 'logged in user session:'
+
+    #2 Executa:
+    resposta = requests.get(
+        url=f'{base_url}/login?username={username}&password={password}',
+        headers=headers
+    )
+    #3 Formata:
     corpo_da_resposta = resposta.json()
     print(resposta)  # Resposta bruta
     print(resposta.status_code)  # Status code
     print(corpo_da_resposta)  # Resposta formatada
 
-    # Valida:
+    #4 Valida:
     assert resposta.status_code == status_code_esperado
     assert corpo_da_resposta['code'] == code_esperado
     assert corpo_da_resposta['type'] == type_esperado
-    assert corpo_da_resposta['message'] == mensagem_esperada
+    #assert corpo_da_resposta['message'] == inicio_mensagem_esperada
+    assert 'logged in user session:' in inicio_mensagem_esperada
 
-##################################################################################
+    #5 Extrair uma informação da mensagem 'logged in user session:'
+    mensagem_recebida = corpo_da_resposta['message']
+    token_usuario = mensagem_recebida[23:37]
+    print(f'O token do usuario eh: {token_usuario}')
+
