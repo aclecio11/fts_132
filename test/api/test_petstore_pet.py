14441df27a8a75bbaa4e162ce8e18cccabbc5e1b
ACLECIO PEREIRA DOS SANTOS <aclecio11@gmail.com>
2022-01-11 21:20:49 -0300

    Criação do metodo GET da entidade Pet

--- test/api/test_petstore_pet.py
+++ test/api/test_petstore_pet.py
@@ -11,9 +11,9 @@ headers = {'Content-Type': 'application/json'}  # Nos .asmx seria 'text/xml'
 def teste_criar_pet():
     # 1 Configura: dados de entrada vem do json
     status_code_esperado = 200
-    id_pet_esperado = 10111
+    pet_id_esperado = 10111
     categoria_esperada = {'id': 1, 'name': 'cachorro'}
-    nome_pet_esperado = 'chocolate'
+    pet_nome_esperado = 'chocolate'
     url_esperada = ['https://static.vix.com/pt/sites/default/files/f/foto-concurso-cachorro-nao-usar-11-0718-1400x1080.jpg']
     tag_esperada = [{'id': 2, 'name': 'vacinado'}]
     status_esperado = 'available'
@@ -33,11 +33,46 @@ def teste_criar_pet():
     # 4 Valida:
     # asserts primários:
     assert resposta.status_code == status_code_esperado
-    assert corpo_da_resposta['id'] == id_pet_esperado
+    assert corpo_da_resposta['id'] == pet_id_esperado
     assert corpo_da_resposta['category'] == categoria_esperada
-    assert corpo_da_resposta['name'] == nome_pet_esperado
+    assert corpo_da_resposta['name'] == pet_nome_esperado
     assert corpo_da_resposta['photoUrls'] == url_esperada
     assert corpo_da_resposta['tags'] == tag_esperada
     assert corpo_da_resposta['status'] == status_esperado
 
 #######################################################################
+
+def testar_consultar_pet():
+    # 1 configura:
+    status_code_esperado = 200
+    pet_id_esperado = 10111
+    categoria_esperada = {'id': 1, 'name': 'cachorro'}
+    pet_nome_esperado = 'chocolate'
+    url_esperada = [
+        'https://static.vix.com/pt/sites/default/files/f/foto-concurso-cachorro-nao-usar-11-0718-1400x1080.jpg']
+    tag_esperada = [{'id': 2, 'name': 'vacinado'}]
+    status_esperado = 'available'
+
+    # 2 Executa:
+    resposta = requests.get(  # Faz a requisição ,passando:
+        url=f'{base_url}/{pet_id_esperado}',  # o end point da API
+        headers=headers  # O header com o Content type
+    )
+
+    # 3 Formatação:
+
+    corpo_da_resposta = resposta.json()
+    print(resposta)  # Resposta bruta
+    print(resposta.status_code)  # Status code
+    print(corpo_da_resposta)  # Resposta formatada
+
+    # 4 Valida:
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['id'] == pet_id_esperado
+    assert corpo_da_resposta['category'] == categoria_esperada
+    assert corpo_da_resposta['name'] == pet_nome_esperado
+    assert corpo_da_resposta['photoUrls'] == url_esperada
+    assert corpo_da_resposta['tags'] == tag_esperada
+    assert corpo_da_resposta['status'] == status_esperado
+
+##################################################################################
